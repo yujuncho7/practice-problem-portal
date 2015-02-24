10692ce7c61d4e9e68cb8e0ab97d05d2d0370105
Yujun Cho <yujuncho7@gmail.com>
2015-02-23 19:35:00 -0800

    added initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practiceproblemportal::Application.config.secret_token = '7953204d56e8fa5a9c206d37242db8f978c2aaf6414f8e9aa9da52684703399522bd4bc488200ec345f339263a698bdf443a66eda683772275671c6556bffdb3'
