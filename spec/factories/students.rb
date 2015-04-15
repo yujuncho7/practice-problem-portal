a45ea08f46dc4466658c2bcbd842a955964a668f
jjojohjohn <jy.johnlee@gmail.com>
2015-04-15 00:38:48 -0300

    coverage good. tests pass for completed problems feature

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -2,4 +2,5 @@ Factory.define :student do |student|
   student.email                 "test@example.com"
   student.password              "password"
   student.password_confirmation "password"
+  student.completed_problems    Hash.new
 end
